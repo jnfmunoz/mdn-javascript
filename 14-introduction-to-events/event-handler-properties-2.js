7b3e7a2ed663de9bfe9ccaf6a3cffcdc4f43d9c4
jnfmunoz <juanmunozgrau@gmail.com>
2025-01-11 03:09:05 -0300

    commit-15

---
+++ 14-introduction-to-events/event-handler-properties-2.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function bgChange() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.onclick = bgChange;

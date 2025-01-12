2f44a8cbaab3b5a010611133cd7d831137d2755b
jnfmunoz <juanmunozgrau@gmail.com>
2025-01-12 07:53:19 -0300

    commit-18

--- 15-event-bubbling/establishing-an-event-detector-in-a-parent-element.js
+++
@@ -1,7 +0,0 @@
-const output = document.querySelector('#output');
-function handleClick(e) {
-    output.textContent += `You clicked on an item ${e.currentTarget.tagName}\n`;
-}
-
-const container = document.querySelector('#container');
-container.addEventListener('click', handleClick);
